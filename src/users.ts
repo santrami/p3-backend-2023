8f2ad416e2b00bf6ea94292554ccae26158f8fab
santrami <santrami@hotmail.com>
2023-05-30 16:27:35 +0200

    mejorando el registro de usuario con bcrypt

--- src/users.ts
+++ src/users.ts
@@ -1,23 +1,28 @@
 import { Router, Request } from "express";
 import prisma from "./prisma-client.js";
 import { errorChecked } from "./utils.js";
-import postsRouter from './posts.js'
-import followRouter from './follow.js';
+import postsRouter from "./posts.js";
+import followRouter from "./follow.js";
+import bcrypt from "bcryptjs"
 
 const router = Router();
 
 // user routes - endpoints
 
 //get all users
-router.get("/", errorChecked(async (req, res) => {
+router.get(
+  "/",
+  errorChecked(async (req, res) => {
     const result = await prisma.user.findMany();
     res.status(200).json(result);
   })
 );
 
 //get one user
-router.get("/:id", errorChecked(async (req, res) => {
-    const { id } = req.params;// no longer needed because of interface RequestWithUserId 
+router.get(
+  "/:id",
+  errorChecked(async (req, res) => {
+    const { id } = req.params; // no longer needed because of interface RequestWithUserId
     const getUser = await prisma.user.findUniqueOrThrow({
       where: { id: Number(id) },
     });
@@ -26,20 +31,37 @@ router.get("/:id", errorChecked(async (req, res) => {
 );
 
 //create a new user
-router.post("/", errorChecked(async (req, res) => {
-    //const { name, surname, username, email, password } = await req.body;
-    const newUser = await prisma.user.create({
-      //or I can just send req.body, not name, surname, email, password.
-      data: req.body,
-      /* name,surname,username,email,password, */
+router.post(
+  "/",
+  errorChecked(async (req, res) => {
+    const userChecked = await prisma.user.findUnique({
+      where: {
+        email: req.body.email,
+      },
     });
-    res.status(200).send(newUser); 
+
+    if (userChecked) {
+      return res.status(409).json({ message: 'usuario ya existe'});
+    }
+    else {
+      const hassedPass= await bcrypt.hash(req.body.password, 10);
+
+      const newUser = await prisma.user.create({
+        data: {
+          ...req.body,
+          password: hassedPass
+        },
+      });
+      res.status(200).send(newUser);
+    }
   })
 );
 
 //update a user
-router.put("/:id", errorChecked(async (req, res) => {
-    const { id } = req.params; // no longer needed because of interface RequestWithUserId 
+router.put(
+  "/:id",
+  errorChecked(async (req, res) => {
+    const { id } = req.params; // no longer needed because of interface RequestWithUserId
     const updateUser = await prisma.user.update({
       where: {
         id: Number(id),
@@ -51,8 +73,10 @@ router.put("/:id", errorChecked(async (req, res) => {
 );
 
 //delete user
-router.delete("/:id", errorChecked(async (req, res) => {
-    const { id } = req.params; // no longer needed because of interface RequestWithUserId 
+router.delete(
+  "/:id",
+  errorChecked(async (req, res) => {
+    const { id } = req.params; // no longer needed because of interface RequestWithUserId
     const deleteUser = await prisma.user.delete({
       where: {
         id: Number(id),
@@ -62,7 +86,7 @@ router.delete("/:id", errorChecked(async (req, res) => {
   })
 );
 
-router.use("/:userId/posts", postsRouter)
-router.use("/:userId/follow", followRouter)
+router.use("/:userId/posts", postsRouter);
+router.use("/:userId/follow", followRouter);
 
 export default router;
